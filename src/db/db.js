aab22a0c4f81642a7b36cdb8e8908d6456614d29
Syed Izhan Hassan <izhanhassan007@gmail.com>
2025-06-07 18:51:50 +0500

    Code Done

--- src/db/db.js
+++ src/db/db.js
@@ -6,11 +6,10 @@ import {DB_NAME} from "../constant.js"
 
 const connectDB = async () => {
     try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-    console.log(`\n MongoDB Connected !! DB Host: ${
-        connectionInstance.connection.host}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB Connected !! DB Host: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MONGODB connection FAILED", error);
+        console.log("MONGODB Connecntion failed...", error);
         process.exit(1)
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // require('dotenv').config({path: './env'})
-import dotenv from "dotenv";
+
+import dotenv from "dotenv"
 import connectDB from "./db/db.js";
 
 dotenv.config({
-    path: './env'
+    path: "./env"
 })
 
 connectDB()
@@ -27,35 +28,26 @@ connectDB()
 
 
 
-
-
-
-/* 
-import express from "express";
+/*
+import express from "express"
 const app = express()
 
 // iffi approach: (ye fuct h)(isko immediatly execute krdo)
-( async () =>{
-    // data base sy jb bh baat ho try catch lagana hi h
+( async () => {
     try {
-    // agr sb sahi raha to,
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)  
-        app.on("error", (error) => {
-            console.log("ERROR: ", error);
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) =>{
+            console.log("ERROR :", error);
             throw error
         })
 
-        app.listen(process.env.PORT, () =>{
-            console.log(`App is listening on port ${process.env.PORT}`);
-            
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on PORT ${process.env.PORT}`);
         })
 
     } catch (error) {
-        console.error("ERROR: ", error)
+        console.error("ERROR :", error)
         throw error
     }
 })()
-
-function connectDB(){}
-connectDB()
 */
